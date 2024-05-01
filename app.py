a8067cc069e77124809c20c9b2241aec2c0c9d26
prach <matej.kratochvil@student.gyarab.cz>
2024-05-01 22:30:06 +0200

    [WIP] 1.1

--- app.py
+++ app.py
@@ -2,38 +2,44 @@ from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
+import os
+import requests
 
 model = load_model('model.h5')
 
 app = Flask(__name__)
 
-def classify_image(image_path):
-    img = load_img(image_path, target_size=(200, 200))
+with open('data/list.txt', 'r') as file:
+    animals = file.read().splitlines()
+
+trans = "https://libretranslate.com/translate"
+
+def classify(path):
+    img = load_img(path, target_size=(200, 200))
     img = img_to_array(img)
     img = img.reshape(1, 200, 200, 3)
     img = img.astype('float32')
     img = img - [123.68, 116.779, 103.939]
     result = model.predict(img)
-    return result[0][0]
+    index = np.argmax(result)
+    animal = animals[index]
+    return animal
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return render_template('index.html', message='No file part')
+            return render_template('index.html', message='Nevybrán žádný soubor!')
         file = request.files['file']
         if file.filename == '':
-            return render_template('index.html', message='No selected file')
+            return render_template('index.html', message='Nevybrán žádný soubor!')
         if file:
             file_path = 'uploads/' + file.filename
             file.save(file_path)
-            result = classify_image(file_path)
-            if result > 0.5:
-                prediction = 'Je to pes!'
-            else:
-                prediction = 'Je to kočka!'
-            return render_template('index.html', message='Obrázek nahrán!', prediction=prediction)
-    return render_template('index.html')
+            result = classify(file_path)
+            return render_template('index.html', message='Obrázek nahrán!', prediction=result)
+    return render_template('index.html', message='', prediction='')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,34 +1,38 @@
 import os
 import numpy as np
-import pandas as pd  # Přidání importu pandas
+import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.optimizers import SGD
 
-dataset_home = 'data/'
+dataset = 'data/'
 
-train_files = [os.path.join(dataset_home, 'train/', f) for f in os.listdir(os.path.join(dataset_home, 'train/')) if f.endswith('.jpg')]
+with open('data/list.txt', 'r') as file:
+    zvirata = file.read().splitlines()
 
-train_labels = [1 if 'dog' in os.path.basename(f) else 0 for f in train_files]
+trainf = []
+trainl = []
+for zvire in zvirata:
+    obrazky = [os.path.join(dataset, 'train', zvire, f) for f in os.listdir(os.path.join(dataset, 'train', zvire)) if f.endswith('.jpg')]
+    trainf.extend(obrazky)
+    trainl.extend([zvire] * len(obrazky))
 
-print('Trenovaci data: ', len(train_files))
+print('Trenovaci data: ', len(trainf))
 
-def define_model():
+def dfn():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
     model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(len(zvirata), activation='softmax'))
     opt = SGD(learning_rate=0.001, momentum=0.9)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 datagen = ImageDataGenerator(rescale=1.0/255.0)
-
-train_df = pd.DataFrame({'filename': train_files, 'class': train_labels})
-train_df['class'] = train_df['class'].astype(str)
+train_df = pd.DataFrame({'filename': trainf, 'class': trainl})
 
 train_it = datagen.flow_from_dataframe(
     dataframe=train_df,
@@ -36,11 +40,9 @@ train_it = datagen.flow_from_dataframe(
     y_col='class',
     target_size=(200, 200),
     batch_size=64,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
-model = define_model()
-
-history = model.fit(train_it, steps_per_epoch=len(train_it), epochs=20, verbose=0)
-
+model = dfn()
+history = model.fit(train_it, steps_per_epoch=len(train_it), epochs=20, verbose=1)
 model.save('model.h5')
