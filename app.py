f674aee0e7660dc0330a0325a7be2a76427dc0c8
Matej Kratochvil <matej.kratochvil@student.gyarab.cz>
2024-05-14 14:38:14 +0200

    [WIP] 1.1 preklad

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import os
 import requests
+from deepl import Translator as Trans
 
 model = load_model('model.h5')
 
 app = Flask(__name__)
 
+translator = Trans("APIKEY")
+
 with open('data/list.txt', 'r') as file:
     animals = file.read().splitlines()
-
-trans = "https://libretranslate.com/translate"
 
 def classify(path):
     img = load_img(path, target_size=(200, 200))
@@ -25,20 +26,19 @@ def classify(path):
     animal = animals[index]
     return animal
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return render_template('index.html', message='Nevybrán žádný soubor!')
+            return render_template('index.html', message=translator.translate_text('No file selected!', target_lang="cs"))
         file = request.files['file']
         if file.filename == '':
-            return render_template('index.html', message='Nevybrán žádný soubor!')
+            return render_template('index.html', message=translator.translate_text('No file selected!'), target_lang="cs")
         if file:
             file_path = 'uploads/' + file.filename
             file.save(file_path)
-            result = classify(file_path)
-            return render_template('index.html', message='Obrázek nahrán!', prediction=result)
+            result = translator.translate_text(classify(file_path), target_lang="cs")
+            return render_template('index.html', message=translator.translate_text('Image uploaded!', target_lang="cs"), prediction=result)
     return render_template('index.html', message='', prediction='')
 
 if __name__ == '__main__':
